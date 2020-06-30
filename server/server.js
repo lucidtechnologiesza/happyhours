80936a7f674652dc8de0ed24a7d75b5027083c7e
Mpho <43447189+Mahloko@users.noreply.github.com>
2020-06-30 23:47:16 +0200

    form on separate page

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /** --- middleware ---- */
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.all('*', (req, res) => {
     res.render('home');
 });
