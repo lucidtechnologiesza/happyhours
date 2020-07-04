05dc09246f6d36aa421ab063896cf5efdbc6c0f8
Jimmy Were <jimmywere@Jimmys-MacBook-Pro.local>
2020-07-05 01:53:38 +0200

    fix db disconnection

--- server/config/db.js
+++ server/config/db.js
@@ -1,27 +1,40 @@
 var mysql = require('mysql');
 require('dotenv/config');
 
-const con = mysql.createConnection(
-  {
-    connectionLimit: 100,
+var db_config =  {
+    connectionLimit: 1000,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    reconnect: true
+    reconnect: true,
   }
-);
-
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
-
-// console.log(con);
-
+  
+  var con = null;
+  function dbConnect() {
+    con = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    con.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(dbConnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    con.on('error', function(err) {
+      //console.log('db error', "reconnecting");
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        dbConnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  dbConnect();
 /*                                        Queries                                                 */
-const Sql = `CREATE TABLE IF NOT EXISTS heroku_b72c9f1df3e1f56.happy_hours(
+const Sql = `CREATE TABLE IF NOT EXISTS heroku_5ae52ed6974f279.happy_hours(
   user_id int(11) NOT NULL AUTO_INCREMENT  PRIMARY KEY,
   aboutUs varchar(50),
   childSurname varchar(50),
@@ -69,12 +82,12 @@ const Sql = `CREATE TABLE IF NOT EXISTS heroku_b72c9f1df3e1f56.happy_hours(
 )`;
 /*                                        End of Queries                                          */
 
-con.connect((connectErr) => {
-  if (connectErr) {
-    console.log(`${connectErr} `)
-    throw connectErr;
-  }
-  console.log("CONNECTED TO DATABASE " + con["database"])
+// con.connect((connectErr) => {
+//   if (connectErr) {
+//     console.log(`${connectErr} `)
+//     throw connectErr;
+//   }
+//   console.log("CONNECTED TO DATABASE " + con["database"])
   
   con.query(
     `${Sql};`,
@@ -83,7 +96,7 @@ con.connect((connectErr) => {
       console.info('Tables created');
     }
   );
-});
+
 
 var HappyHours = {};
 
@@ -99,5 +112,5 @@ HappyHours.insert = function (data) {
     )
   });
 }
-
+// con.query('select 1 + 1', (err, rows) => { /* */ });
 module.exports = HappyHours;
