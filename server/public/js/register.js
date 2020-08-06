552ac6f1ac44e83c63f71bb754d248d4c6a40cd2
Jimmy Were <jimmywere@Jimmys-MacBook-Pro.local>
2020-08-06 22:58:01 +0200

    next-btn form validation RADIO_BTNS NOT DONE

--- server/public/js/register.js
+++ server/public/js/register.js
@@ -1,44 +1,83 @@
 //jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
+
+function validateAll(formNumber){
+    var inputs, index, selections;
+    var valid = true;
+    inputs = document.getElementById("form" + formNumber).getElementsByTagName('input');
+    selections = document.getElementById("form" + formNumber).getElementsByTagName('select');
+    //var radioBtns = document.getElementById("form" + formNumber).getElementsByName('Categories');
+    for (index = 0; index < inputs.length; ++index) {
+        var values = inputs[index].value.trim();
+        if (values.length == 0){
+            inputs[index].style.backgroundColor = "red";
+            valid = false;
+        } else{
+            inputs[index].style.backgroundColor = "";
+        }
+    }
+    for (index = 0; index < selections.length; ++index) {
+        var opt = selections[index];
+        if (opt.options[opt.selectedIndex].value == ""){
+            selections[index].style.backgroundColor = "red";
+            valid = false;
+        } else{
+            selections[index].style.backgroundColor = "";
+        }
+    }
+    //return (true);
+    return (valid);
+}
+
+$('input[name="agree"]').click(function(){
+    $(this).css('background-color','green');
+    $('input[name="disagree"]').css('background-color', 'transparent');
+});
+
+$('input[name="disagree"]').click(function(){
+    $(this).css('background-color','orange');
+    $('input[name="agree"]').css('background-color', 'transparent');
+});
+
 var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function() {
-    if (animating) return false;
-    animating = true;
+    if (validateAll(event.target.id)){
+        if (animating) return false;
+        animating = true;
 
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
 
-    //activate next step on progressbar using the index of next_fs
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({ opacity: 0 }, {
-        step: function(now, mx) {
-            //as the opacity of current_fs reduces to 0 - stored in "now"
-            //1. scale current_fs down to 80%
-            scale = 1 - (1 - now) * 0.2;
-            //2. bring next_fs from the right(50%)
-            left = (now * 50) + "%";
-            //3. increase opacity of next_fs to 1 as it moves in
-            opacity = 1 - now;
-            current_fs.css({
-                'transform': 'scale(' + scale + ')',
-                'position': 'absolute'
-            });
-            next_fs.css({ 'left': left, 'opacity': opacity });
-        },
-        duration: 800,
-        complete: function() {
-            current_fs.hide();
-            animating = false;
-        },
-    });
+        //activate next step on progressbar using the index of next_fs
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({ opacity: 0 }, {
+            step: function(now, mx) {
+                //as the opacity of current_fs reduces to 0 - stored in "now"
+                //1. scale current_fs down to 80%
+                scale = 1 - (1 - now) * 0.2;
+                //2. bring next_fs from the right(50%)
+                left = (now * 50) + "%";
+                //3. increase opacity of next_fs to 1 as it moves in
+                opacity = 1 - now;
+                current_fs.css({
+                    'transform': 'scale(' + scale + ')',
+                    'position': 'absolute'
+                });
+                next_fs.css({ 'left': left, 'opacity': opacity });
+            },
+            duration: 800,
+            complete: function() {
+                current_fs.hide();
+                animating = false;
+            },
+        });
+    }
 });
-
 $(".previous").click(function() {
     if (animating) return false;
     animating = true;
@@ -72,8 +111,6 @@ $(".previous").click(function() {
 
     });
 });
-
-
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
